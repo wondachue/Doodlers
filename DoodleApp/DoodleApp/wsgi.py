82c55793c3288a4dca730ad0aba9af3085b06faf
wondachue <megddwyer@gmail.com>
2015-09-26 14:56:40 -0700

    Project do-over

--- DoodleApp/DoodleApp/wsgi.py
+++ DoodleApp/DoodleApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for call911 project.
+WSGI config for DoodleApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "call911.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DoodleApp.settings")
 
 application = get_wsgi_application()
