f0d4c869d20b04b6845c8068d39b772e5da6a5ce
RodSalazar <rodrigos@fb.com>
2015-09-26 19:09:43 -0700

    Censorship in the comments list

--- DoodleApp/Doodlers/views.py
+++ DoodleApp/Doodlers/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Doodle, Comment
 from django.template import RequestContext, loader
+
+import sentiment
+
 # Create your views here.
 def index(request):
     latest_doodles = Doodle.objects.order_by('-pub_date')[:5]
@@ -14,9 +17,19 @@ def doodle(request, id):
 	response = "This is where %s doodle goes. Expect to see comments here" % Doodle.objects.get(pk=id).title
 	template = loader.get_template('Doodlers/doodle.html')
 	current_doodle = Doodle.objects.get(pk=id);
+	comments = current_doodle.comment_set.all()
+	scores = {}
+	num_bad = 0
+	for comment in comments:
+		scores[comment.id] = sentiment.getSentiment(comment.comment_text)
+		if scores[comment.id] < 0:
+			num_bad += 1
+
 	context = RequestContext(request, {
 		'doodle': current_doodle.title,
-		'comments': current_doodle.comment_set.all()
+		'comments': comments,
+		'scores': scores,
+		'num_bad': num_bad,
 	})
 	return render(request, 'Doodlers/doodle.html', context)
 def comment(request, id):
